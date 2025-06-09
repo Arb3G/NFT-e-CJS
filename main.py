94e53134c95acfeabe9afd7ee095f71324d7b9b0
Arb3G <rbg242570@gmail.com>
2025-06-09 18:23:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()  # Loads variables from .env
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
