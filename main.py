e910891c28ef88d405ec957f39649711dcdb1714
Arb3G <rbg242570@gmail.com>
2025-06-09 22:34:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import discord
 from discord.ext import commands
 import os
 
+from discord.ext import commands
+from discord import app_commands
+
 intents = discord.Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
