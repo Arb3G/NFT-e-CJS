6d4ed7ee71d3dd75685915bea0a2633ad8e23733
Arb3G <rbg242570@gmail.com>
2025-06-30 10:14:30 -0400

    Update mintnft.js
    Adding minting as a future option

--- commands/mintnft.js
+++ commands/mintnft.js
@@ -1,1 +1,16 @@
+// mintnft.js
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('mintnft')
+    .setDescription('Mint your AI art as an NFT (placeholder for now)'),
+
+  async execute(interaction) {
+    await interaction.reply({
+      content: '🚧 NFT minting is not available yet. Stay tuned!',
+      ephemeral: true
+    });
+  }
+};
 
