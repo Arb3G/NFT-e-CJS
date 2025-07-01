ce580bc58e6810ced6aa18b382dccb7375bce810
Arb3G <rbg242570@gmail.com>
2025-07-01 11:20:06 -0400

    Create test-deploy.js

---
+++ test-deploy.js
@@ -0,0 +1,21 @@
+const { REST, Routes } = require('discord.js');
+const token = process.env.DISCORD_TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID; // if using guild commands
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
