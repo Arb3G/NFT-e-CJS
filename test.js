b6e4eb26ce5978ad080cd180b91c260b7165e15a
Arb3G <rbg242570@gmail.com>
2025-07-01 20:59:22 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 (async () => {
   try {
     //const data = await rest.get(Routes.applicationCommands(process.env.CLIENT_ID));
-    const data = await rest.get(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID));
+    const data = await rest.get(Routes.applicationGuildCommands(process.env.CLIENT_ID,process.env.GUILD_ID));
     console.log('✅ Successfully fetched commands:', data);
   } catch (error) {
     console.error('❌ GET request failed:', error);
