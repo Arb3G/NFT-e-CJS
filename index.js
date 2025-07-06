4c23ecc1a303595f2d5a4dedb0c82de926fafe1e
Arb3G <rbg242570@gmail.com>
2025-07-06 15:13:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   res.send('✅ Discord bot and web server are running.');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🌐 Express server running at http://localhost:${PORT}`);
 });
